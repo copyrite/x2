9188131e136f2729d2f3f17873ac2ddc57dd8ff8
copyrite <tatu.heikki.leinonen@gmail.com>
2025-02-23 00:00:15 +0200

    Refactor according to x2py's update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import x2py
 
 if __name__ == "__main__":
-    x2 = x2py.X2(os.getenv("XCOM2CONTENTPATH"))
+    x2 = x2py.X2(os.getenv("XCOM2CONTENTPATH"), os.getenv("XCOM2CONTENTPATH"))
     Path("_data").mkdir(exist_ok=True)
     with open("_data/wotc.json", "w") as file:
         x2.dump(file, html=True)
@@ -16,24 +16,20 @@ if __name__ == "__main__":
     Path("_wotc").mkdir(exist_ok=True)
     for kind, manager in x2.managers.items():
 
-        with open(Path(f"_wotc/x2{kind}templatemanager.html"), "w") as file:
+        with open(Path(f"_wotc/{kind}.html"), "w") as file:
             manager_layout_kind = (
-                kind
-                if Path(f"_layouts/x2{kind}templatemanager.html").exists()
-                else "data"
+                kind if Path(f"_layouts/{kind}.html").exists() else "x2datatemplate"
             )
             file.write(
-                f"---\ntitle: {kind}\nlayout: x2{manager_layout_kind}templatemanager\ntemplate_type: {kind}\nflavor: wotc\npermalink: wotc/{kind}\n---\n"
+                f"---\ntitle: {kind}\nlayout: {manager_layout_kind}\ntemplate_type: {kind}\nflavor: wotc\npermalink: wotc/{kind}\n---\n"
             )
 
         template_layout_kind = (
-            kind if Path(f"_layouts/x2{kind}template.html").exists() else "data"
+            f"{kind}_" if Path(f"_layouts/{kind}_.html").exists() else "x2datatemplate_"
         )
 
         for template in manager:
-            with open(
-                Path(f"_wotc/") / f"{template}_x2{kind}template.html", "w"
-            ) as file:
+            with open(Path(f"_wotc/") / f"{kind}_{template}.html", "w") as file:
                 file.write(
-                    f"---\ntitle: {template}\nlayout: x2{template_layout_kind}template\ntemplate_type: {kind}\npermalink: wotc/{kind}/{template}\nflavor: wotc\n---\n"
+                    f"---\ntitle: {template}\nlayout: {template_layout_kind}\ntemplate_type: {kind}\npermalink: wotc/{kind}/{template}\nflavor: wotc\n---\n"
                 )
